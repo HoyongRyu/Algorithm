058b76112488e6a0e13b10940789d3101f83d440
HoyongRyu <luvfool0927@gmail.com>
2016-03-04 17:08:38 +0900

    .

---
+++ AlgorithmStudy/src/leetcode/ing/PowerOfThree.java
@@ -0,0 +1,27 @@
+package leetcode.ing;
+
+public class PowerOfThree {
+
+	public static void main(String[] args) {
+		PowerOfThree pot = new PowerOfThree();
+		
+		System.out.println(pot.isPowerOfThree(3));
+		System.out.println(pot.isPowerOfThree(45));
+		System.out.println(pot.isPowerOfThree(20));
+		System.out.println(pot.isPowerOfThree(9));
+		System.out.println(pot.isPowerOfThree(2147483647));
+	}
+	
+	public boolean isPowerOfThree(int n) {
+		if (n==0){
+			return false;
+		}
+		
+		boolean result = false;
+
+		
+		
+		return result;
+    }
+
+}
