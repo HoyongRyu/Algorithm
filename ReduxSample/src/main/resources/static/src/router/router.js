378ad4ca049b57f76db08de83d8ca8b41d6ab2d5
HoyongRyu <luvfool0927@gmail.com>
2016-03-28 18:02:13 +0900

    좌메뉴 해보자

--- ReduxSample/src/main/resources/static/src/router/router.js
+++ ReduxSample/src/main/resources/static/src/router/router.js
@@ -3,7 +3,7 @@ import { Route, Redirect, IndexRoute } from 'react-router';
 
 import Index from 'ui/container/Index';
 import SignIn from 'ui/container/user/SignInContainer';
-import App from 'ui/container/App';
+import Admin from 'ui/container/Admin';
 import SignUp from 'ui/container/user/SignUpContainer';
 import MemberList from 'ui/container/MemberListContainer';
 
@@ -12,7 +12,8 @@ export default () => (
   <Route path="/" name="index" component={Index}>
     <IndexRoute component={SignIn}/>
     <Route path="/signup" name="signup" component={SignUp}/>
-  	<Route path="/admin" name="app" component={App}>
+  	<Route path="/admin" name="admin" component={Admin}>
+  	  /* index router에 Dashboard 추가 */
       <Route path="/member" name="member" component={MemberList} />
     </Route>
   </Route>
--- ReduxSample/src/main/resources/static/src/ui/container/App.js
+++ ReduxSample/src/main/resources/static/src/ui/container/App.js
@@ -3,41 +3,6 @@ import { Link, Router } from 'react-router';
 import { connect } from 'react-redux';
 import { Nav, Navbar, NavItem, Button, Jumbotron, Grid, Row, Col } from 'react-bootstrap'
 
-const LoginForm = (props) => {
-  return (
-    props.isAuthenticated ? '' :
-    <Jumbotron>
-      <Grid>
-        <Row>
-          <Col md={7}>
-            <h2 className="text-center"><img src={require('img/logobas.jpg')} /></h2>
-            <h1 className="text-center text-muted">Welcome to</h1>
-            <h3 className="text-center text-muted">
-              <b>Administration Site</b>
-            </h3>
-          </Col>
-          <Col md={5}>
-            <form role="form" className="text-left" submit="">
-              <div className="form-group" id="loginId">
-                <label className="control-label">Email address</label>
-                <input className="form-control" id="loginId"
-                placeholder="Enter email" type="email" />
-              </div>
-              <div className="form-group" id="password">
-                <label className="control-label">Password</label>
-                <input className="form-control" id="password"
-                placeholder="Password" type="password" />
-              </div>
-              <Button type="submit" bsStyle="default">Sign In</Button>&nbsp;
-              <Link className="btn btn-primary" to="/signup">Sign Up</Link>
-            </form>
-          </Col>
-        </Row>
-      </Grid>
-    </Jumbotron>
-  );
-}
-
 
 export class App extends Component {
   
@@ -46,15 +11,54 @@ export class App extends Component {
   }
 
   render() {
-    const topMenuItems = [
-      {label: 'Home', link: '/'},
-      {label: 'Member', link: '/member'}
+    const leftMenuItems = [
+      {label: 'Member', link: '/member'},
+      {label: 'Content', link: '/content'},
+      {label: 'Display', link: '/display'},
+      {label: 'Order', link: '/order'},      
+      {label: 'System', link: '/system'}
     ];
 
     return (
-      <div id="application">
-        <LoginForm />
-        {this.props.children}
+      <div class="row">
+        <div class="col-sm-3">
+          <div class="sidebar-nav">
+            <div class="navbar navbar-default" role="navigation">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <span class="visible-xs navbar-brand">Sidebar menu</span>
+              </div>
+              <div class="navbar-collapse collapse sidebar-navbar-collapse">
+                <ul class="nav navbar-nav">
+                  <li class="active"><a href="#">Menu Item 1</a></li>
+                  <li><a href="#">Menu Item 2</a></li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Action</a></li>
+                      <li><a href="#">Another action</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li class="divider"></li>
+                      <li class="dropdown-header">Nav header</li>
+                      <li><a href="#">Separated link</a></li>
+                      <li><a href="#">One more separated link</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Menu Item 4</a></li>
+                  <li><a href="#">Reviews <span class="badge">1,118</span></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-9">
+          {this.props.children}
+        </div>
       </div>
     );
   }
