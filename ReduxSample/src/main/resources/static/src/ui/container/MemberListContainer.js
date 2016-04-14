b856028bdd3b8957a0ab054c1ab311028d3bbe64
HoyongRyu <luvfool0927@gmail.com>
2016-04-14 15:08:51 +0900

    view 삭제

--- ReduxSample/src/main/resources/static/src/ui/container/MemberListContainer.js
+++
@@ -1,9 +0,0 @@
-import MemberListComponent from 'ui/component/member/MemberListComponent';
-import {connect} from 'react-redux';
-import {getMemberList} from 'reducers/member/memberListReducer';
-
-
-export default connect(
-  state => ({members: state.memberListReducer.members}),
-  {getMemberList}
-)(MemberListComponent);
